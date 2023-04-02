76b545ed7589e55988bb7218a75b65a6afdb3a29
Justin Liu <liujustin53@gmail.com>
2023-04-02 03:21:48 -0400

    heal spell
    also moved around player component order

---
+++ Assets/Scripts/Spells/HealSpell.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealSpell : Spell
+{
+    [SerializeField]
+    private int healAmount = 10;
+
+    private Mortal player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Mortal>(); 
+    }
+
+    protected override void Fire()
+    {
+        player.Heal(healAmount);
+    }
+}
